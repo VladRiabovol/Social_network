2236478c64a16d9cf595f9e00a7f212e7b59b3b4
VladRiabovol <82464669+VladRiabovol@users.noreply.github.com>
2021-12-25 17:11:02 +0200

    refactor code

--- social_network/user/views.py
+++ social_network/user/views.py
@@ -1,7 +1,5 @@
-from rest_framework.views import APIView
-from rest_framework.generics import ListAPIView, RetrieveAPIView
-from rest_framework.response import Response
-from rest_framework import authentication, permissions
+from rest_framework.generics import RetrieveAPIView
+from rest_framework import permissions
 from .models import User
 from .serializers import UserActivitySerializer
 
